82543775b0996b1faaa2eac48a16e6c2b64ab4c6
SSHUSSAIN <shehzad.hussain86@yahoo.com>
2021-03-23 23:38:37 +0500

    Tested

--- src/com/company/MarupekeGrid.java
+++ src/com/company/MarupekeGrid.java
@@ -6,55 +6,54 @@ public class MarupekeGrid {
 
     // Marupeke grid field declaration
     int size;
-    char[][]grid;
+    char[][] grid;
 
     //Default constructor
-    public MarupekeGrid(){}
+    public MarupekeGrid() {
+    }
 
     // Constructor takes size as parameter
-    public MarupekeGrid(int size){
+    public MarupekeGrid(int size) {
 
         //condition to check if the size is greater than 10 we set the default to size 10
-        if(size>10){
-            this.size=10;
-            this.grid= new char [10][10];
+        if (size > 10) {
+            this.size = 10;
+            this.grid = new char[10][10];
         }
 
         //condition to check if the size is less than 3 we set the default to size 3
-        else if(size<3){
-            this.size=3;
-            this.grid= new char [3][3];
-        }
-        else{
-            this.size=size;
-            this.grid= new char [size][size];
+        else if (size < 3) {
+            this.size = 3;
+            this.grid = new char[3][3];
+        } else {
+            this.size = size;
+            this.grid = new char[size][size];
         }
 
     }
 
     //setSolid method
-    boolean setSolid(int x, int y){
-        if(grid[x][y] == 'x' || grid[x][y] == 'o'){
+    boolean setSolid(int x, int y) {
+        if (grid[x][y] == 'x' || grid[x][y] == 'o') {
             return false;
-        }
-        else{
+        } else {
+            grid[x][y]='#';
             return true;
         }
 
     }
 
     //setX method to enter 'X' on the grid location x and y
-    boolean setX(int x, int y, boolean canEdit){
-        if(canEdit){
-            grid[x][y]='x';
+    boolean setX(int x, int y, boolean canEdit) {
+        if (canEdit) {
+            grid[x][y] = 'x';
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    //setO method to enter 'X' on the grid location x and y
+    //setO method to enter 'O' on the grid location x and y
     boolean setO(int x, int y, boolean canEdit) {
         if (canEdit) {
             grid[x][y] = 'o';
@@ -64,27 +63,68 @@ public class MarupekeGrid {
         }
     }
 
-            public static void main(String[] args) {
-            // write your code here
+    public static MarupekeGrid randomPuzzle(int size, int numFill, int numX, int numO) {
 
-                boolean [] [] editable = new boolean [10] [10];
+        boolean[][] editable = new boolean[10][10];
 
-                /*  This reads the size of grid
-                 *  provided by player
-                 */
-                Scanner scan = new Scanner(System.in);
-                System.out.print("Enter size of grid: ");
+        if (numFill > size * size) {
+            return null;
+        } else {
 
-                // This method reads the number provided using keyboard
-                int size = scan.nextInt();
+            // Displaying the number
+            System.out.println("The size of grid entered by player is: " + size);
+            MarupekeGrid marupekeGrid = new MarupekeGrid(size);
+            System.out.println("The size of MarupekeGrid grid is: " + marupekeGrid.size);
 
-                // Closing Scanner after the use
-                scan.close();
 
-                // Displaying the number
-                System.out.println("The size of grid entered by player is: "+size);
-                MarupekeGrid marupekeGrid = new MarupekeGrid(size);
-                System.out.println("The size of MarupekeGrid grid is: "+marupekeGrid.size);
+            System.out.println("is Set0 edited: " + marupekeGrid.setO(2, 2, true));
+            System.out.println("is SetX edited: " + marupekeGrid.setX(3, 3, true));
 
+
+            return marupekeGrid;
         }
+
+    }
+
+    // toString method to convert MarupekeGrid in proper format
+    public static void toString(MarupekeGrid puzzleToString, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (puzzleToString.grid[i][j] == '\0') {
+                    System.out.print("_");
+                } else {
+                    System.out.print(puzzleToString.grid[i][j]);
+                }
+
+            }
+            System.out.print("\n\r");
+        }
+    }
+
+
+    public static void main(String[] args){
+        int size;
+
+        /*  This reads the size of grid
+         *  provided by player
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter size of grid: ");
+
+        // This method reads the number provided
+        size = scan.nextInt();
+
+        // Closing Scanner after the use
+        scan.close();
+
+        //Instantiating marupekeGrid
+        MarupekeGrid marupekeGrid = randomPuzzle(size,2,1,1);
+
+        //testing solid method
+        marupekeGrid.setSolid(4,4);
+
+        //Calling Puzzle to string method
+        toString(marupekeGrid, size);
+
+    }
 }
